0c77aabf503dcc401933e11b2295b108371cc0b2
ruerue01 <168451587+ruerue01@users.noreply.github.com>
2024-06-14 23:53:36 +0200

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,68 @@
+//question number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "What fruit is dried to produce raisins?",
+        answer: "Grapes",
+        options: [
+            "Apples",
+            "Grapes",
+            "Peaches",
+            "Plums"
+        ]
+    },
+    {
+        numb: 2,
+        question: "I am medium-sized and round. I can be red or green. What am i?",
+        answer: "Apple",
+        options: [
+            "Banana",
+            "Strawberry",
+            "Watermelon",
+            "Apple"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which of these ISN'T a real type of fruit?",
+        answer: "Boogletang",
+        options: [
+            "Boogletang",
+            "Lychee",
+            "Apricot",
+            "Pomegranate"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Pineapples are grown all over the world. But where are they from originally?",
+        answer: "South America",
+        options: [
+            "The Moon",
+            "Japan",
+            "France",
+            "South America"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What fruit are monkeys known to enjoy?",
+        answer: "Bananas",
+        options: [
+            "Bananas",
+            "Cherries",
+            "Apples",
+            "Pears"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What colour are Blackberries?",
+        answer: "Black",
+        options: [
+            "Red",
+            "Green",
+            "Maroon",
+            "Black"
+        ]
+    },
+];
